67af22dccb19546706f88d9b07891ceb2e3f0891
Spencer Haka <spencer.haka@gmail.com>
2024-09-10 15:45:00 -0600

    Update date function for current date on POST

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ const routes = (app) => {
     //   complete: true
     // }
     .post((req, res) => {
-      const today = new Date().toISOString().slice(0, 10)
+      const today = dateFunc()
+
+      function dateFunc(date = new Date()) {
+        const yyyy = date.getFullYear()
+        let mm = date.getMonth() + 1
+        let dd = date.getDate()
+    
+        // if (dd < 10) dd = '0' + dd
+        // if (mm < 10) mm = '0' + mm
+    
+        return `${dd}/${mm}/${yyyy}`
+      }
 
       try {
         const todo = req.body
